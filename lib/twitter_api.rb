47264c816805efffc1b8db2135bc297cc73589fc
marat <marat.tynarbekov@iaau.edu.kg>
2017-07-11 14:27:46 +0600

    Connect rails app with Twitter Api

---
+++ lib/twitter_api.rb
@@ -0,0 +1,17 @@
+class TwitterApi
+
+	def self.our_public_tweets
+		client.user_timeline('tynarbekov95')
+	end
+
+
+	def self.client
+		@client ||= Twitter::REST::Client.new do |config|
+		config.consumer_key        = "CH0dYpLIRK8OkmI0A5NRGalpk"
+		config.consumer_secret     = "8l9XEe3JWRPqZfF763jsC3cT3V2FRCMeHtRkG7YTcCnCDmqvi0"
+		config.access_token        = "884131760430354432-kdd8Av7hnOc1RJb0ljLVDQrzpvxp716"
+		config.access_token_secret = "uEO0l3IVjoiaWk9DYgYgAHBh5tR1QypNsOENqqMt2D3CQ"
+		end
+	end	
+
+end
